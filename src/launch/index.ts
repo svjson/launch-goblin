89ad737eed3f9e98138384c1bc85d5417519c101
Sven Johansson <sven.johansson@iteam.se>
2025-08-29 12:04:09 +0200

    Ensure lauching can only happen once per invocation

--- src/launch/index.ts
+++ src/launch/index.ts
@@ -1,19 +1,31 @@
 import { ProjectComponent } from '@src/project'
 import { spawn } from 'node:child_process'
 
+let launched = false
+
 export const launch = async (components: ProjectComponent[]) => {
-  const child = spawn(
-    'npx',
-    [
-      'turbo',
-      'run',
-      'dev',
-      ...components.flatMap((c) => ['--filter', c.package]),
-    ],
-    {
-      stdio: 'inherit',
-    }
-  )
+  if (launched) return
+  launched = true
+
+  const bin = 'npx'
+  const args = [
+    'turbo',
+    'run',
+    'dev',
+    ...components.flatMap((c) => ['--filter', c.package]),
+  ]
+
+  const child = spawn(bin, args, {
+    stdio: 'inherit',
+  })
+
+  process.on('SIGINT', () => {
+    child.kill('SIGINT')
+  })
+
+  process.on('SIGTERM', () => {
+    child.kill('SIGINT')
+  })
 
   child.on('exit', () => {
     process.exit(0)
